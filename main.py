7cfd1743cc9d5b26ab4fafe49516bbf9ef2ec657
Victor Petukhov <i@victor.am>
2018-01-31 00:15:57 +0300

    Added Kotlin custom compiler (binaries) for CST printing

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ parser.add_argument('--input_folder', '-i', nargs=1, type=str, help='folder with
 parser.add_argument('--output_folder', '-o', nargs=1, type=str, help='output folder with AST as JSON')
 
 kt_code_temp_file = 'code.kt'
+compiler_path = './lib/kotlinc/bin/kotlinc'
 
 args = parser.parse_args()
-compiler_path = args.compiler_path[0]
 input_folder = args.input_folder[0]
 output_folder = args.output_folder[0]
 
