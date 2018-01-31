d96a38ed4c9f33e2ff4650ba9d1b4f98d37f3da9
Victor Petukhov <i@victor.am>
2018-01-31 23:19:00 +0300

    source2ast code moved to source2ast.py

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import argparse
 
-from lib.KotlinSource2AstConverter import KotlinSource2AstConverter
-from lib.helpers.TimeLogger import TimeLogger
-
+from source2ast import source2ast
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_folder', '-i', nargs=1, type=str, help='folder with kotlin source codes')
 parser.add_argument('--output_folder', '-o', nargs=1, type=str, help='output folder with AST as JSON')
 
-kt_code_temp_file = 'code.kt'
-compiler_path = './lib/kotlinc/bin/kotlinc'
-
 args = parser.parse_args()
 input_folder = args.input_folder[0]
 output_folder = args.output_folder[0]
 
-time_logger = TimeLogger()
-
-KotlinSource2AstConverter.convert(compiler_path, input_folder, output_folder)
-
-time_logger.finish(task_name='Parsing')
+source2ast(input_folder, output_folder)
---
+++ source2ast.py
@@ -0,0 +1,14 @@
+from lib.KotlinSource2AstConverter import KotlinSource2AstConverter
+from lib.helpers.TimeLogger import TimeLogger
+
+
+kt_code_temp_file = 'code.kt'
+compiler_path = './lib/kotlinc/bin/kotlinc'
+
+
+def source2ast(input_folder, output_folder):
+    time_logger = TimeLogger()
+
+    KotlinSource2AstConverter.convert(compiler_path, input_folder, output_folder)
+
+    time_logger.finish(task_name='Parsing')
