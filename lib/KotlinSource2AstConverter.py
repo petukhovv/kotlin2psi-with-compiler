03382a546df6d567d256e1af5e279c6a6ed1ee13
Victor Petukhov <i@victor.am>
2018-02-01 01:41:49 +0300

    Removed unused counter

--- lib/KotlinSource2AstConverter.py
+++ lib/KotlinSource2AstConverter.py
@@ -9,10 +9,8 @@ from .helpers.AstHelper import AstHelper
 class KotlinSource2AstConverter:
     @staticmethod
     def convert(compiler_path, input_folder, output_folder):
-        params = {'counter': 1}
-
         def file_process(filename):
-            time_logger = TimeLogger(task_name='Parsing %d file' % params['counter'])
+            time_logger = TimeLogger(task_name='Parsing %s file' % filename)
             subprocess.call([compiler_path, filename])
 
             relative_filename = os.path.relpath(filename, input_folder)
@@ -27,7 +25,5 @@ class KotlinSource2AstConverter:
                 f.write(ast)
 
             time_logger.finish()
-
-            params['counter'] += 1
 
         FilesWalker.walk(input_folder, file_process)
