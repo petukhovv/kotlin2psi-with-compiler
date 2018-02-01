c1534b2d118bf98c881fbefd09d6483e458446f1
vapetukhov <vapetukhov@yamoney.ru>
2018-02-01 15:06:28 +0300

    Added counting source code files

--- lib/KotlinSource2AstConverter.py
+++ lib/KotlinSource2AstConverter.py
@@ -9,8 +9,19 @@ from .helpers.AstHelper import AstHelper
 class KotlinSource2AstConverter:
     @staticmethod
     def convert(compiler_path, input_folder, output_folder):
+        files_info = {
+            'all': 0,
+            'current': 0
+        }
+
+        def files_count(filename):
+            files_info['all'] += 1
+
         def file_process(filename):
-            time_logger = TimeLogger(task_name='Parsing %s file' % filename)
+            files_info['current'] += 1
+
+            time_logger = TimeLogger(task_name='Parsing %d of %d file: %s'
+                                               % (files_info['current'], files_info['all'], filename))
             subprocess.call([compiler_path, filename])
 
             relative_filename = os.path.relpath(filename, input_folder)
@@ -26,4 +37,8 @@ class KotlinSource2AstConverter:
 
             time_logger.finish()
 
+        # Count source code files stage
+        FilesWalker.walk(input_folder, files_count, extension='kt')
+
+        # Parsing source code files stage
         FilesWalker.walk(input_folder, file_process, extension='kt')
